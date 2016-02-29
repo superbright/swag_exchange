a0229e32d3a1b05edfa691f7b7fa2a3575013825
paul <paul.christophe6@gmail.com>
2016-02-29 13:41:25 -0500

    please return

--- routes/index.js
+++ routes/index.js
@@ -3,23 +3,23 @@ var router = express.Router();
 
 /* GET home page, animated url */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'SWAG' });
 });
 
 // return tablepage
-router.get('/return_tablet', function(req, res, next) {
-  res.render('return_tablet', { title: 'Express' });
+router.get('/return', function(req, res, next) {
+  res.render('return', { title: 'SWAG' });
 });
 
 // swag category submission form
 router.get('/swag', function(req, res, next) {
-  res.render('swag_submit', { title: 'Express' });
+  res.render('swag_submit', { title: 'SWAG' });
 });
 
 router.post('/swag_submit', function(req, res) {
     var category = req.body.category;
     console.log(category);
-   res.render('swag_submit', { title: 'Express' });
+   res.render('swag_submit', { title: 'SWAG' });
 });
 
 //user register post call
@@ -28,7 +28,7 @@ router.post('/user_register', function(req, res) {
     var lastname = req.body.lastname;
     var email = req.body.email;
     console.log(name);
-   res.render('return_tablet', { title: 'Express' });
+   res.render('return_tablet', { title: 'SWAG' });
 });
 
 module.exports = router;
